494c2f347bc3029e11eece6d142188b683a9b00a
senku <senku.kz@gmail.com>
2023-05-10 20:11:42 +0600

    rewrite on deploy by pom and jar file only. without parsing xml.

--- src/main/java/PomJarFile.java
+++ src/main/java/PomJarFile.java
@@ -1,24 +1,46 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PomJarFile {
-    private final RunMvnFromJava runMvnFromJava = new RunMvnFromJava();
-    private final XmlFile xmlFile = new XmlFile();
+    public void workWithDirectory(String repositoryId, String urlNexus, String srcDir){
+        Set<String> fileSet = this.getPomSet(srcDir);
 
-    public PomJarFile() {
+        for (String filename : fileSet) {
+            System.out.println("===========================================");
+            String srcFilePom = srcDir + "/" + filename;
+            System.out.println(srcFilePom);
+
+            String srcFileJar = srcFilePom.substring(0, srcFilePom.length() - 3) + "jar";
+            boolean c = new File(srcFileJar).isFile();
+            System.out.printf("%s\t%s%n", c, srcFileJar);
+            if (c) {
+                this.runMvnCommand(repositoryId, urlNexus, srcFilePom, srcFileJar);
+            }
+        }
+        System.out.println("===========================================");
+
     }
+
+
+    public Set<String> getPomSet(String dir) {
+        return Stream.of(new File(dir).listFiles())
+                .filter(file -> !file.isDirectory())
+                .map(File::getName)
+                .filter(name -> name.endsWith(".pom"))
+                .collect(Collectors.toSet());
+    }
+
 
     public void runMvnCommand(String repositoryId, String urlNexus, String pomFile, String jarFile){
         /*
-            mvn deploy:deploy-file \
-                -DgroupId=<group-id> \
-                -DartifactId=<artifact-id> \
-                -Dversion=<version> \
-                -Dpackaging=<type-of-packaging> \
-                -Dfile=<path-to-file> \
+            mvn deploy:deploy-file -DgeneratePom=false \
                 -DrepositoryId=<id-to-map-on-server-section-of-settings.xml> \
-                -Durl=<url-of-the-repository-to-deploy>
+                -Durl=<url-of-the-repository-to-deploy> \
+                -DpomFile=<path-to-pom-file>  \
+                -Dfile=<path-to-file>
 
             mvn deploy:deploy-file -DgeneratePom=false
                 -DrepositoryId=localnexus
@@ -41,34 +63,16 @@ public class PomJarFile {
             System.out.println(cmd);
 
             // Run and get the output.
-            String outlist[] = runMvnFromJava.runCommand(cmd);
+            String[] outList = RunMvnFromJava.runCommand(cmd);
+
             // Print the output to screen character by character.
             // Safe and not very inefficient.
-            for (String el:outlist){
+            for (String el:outList){
                 System.out.println(el);
             }
         } catch (IOException e) {
             System.err.println(e);
         }
-
-    }
-
-    public void workWithDirectory(String repositoryId, String urlNexus, String srcDir){
-        Set<String> fileSet = xmlFile.getPomSet(srcDir);
-
-        for (String filename : fileSet) {
-            System.out.println("===========================================");
-            String srcFilePom = srcDir + "/" + filename;
-            System.out.println(srcFilePom);
-
-            String srcFileJar = srcFilePom.substring(0, srcFilePom.length() - 3) + "jar";
-            boolean c = new File(srcFileJar).isFile();
-            System.out.printf("%s\t%s%n", c, srcFileJar);
-            if (c) {
-                this.runMvnCommand(repositoryId, urlNexus, srcFilePom, srcFileJar);
-            }
-        }
-        System.out.println("===========================================");
 
     }
 
